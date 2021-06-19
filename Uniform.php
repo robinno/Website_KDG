dcad45c6e515d9df7fc52c1fc3e9d459ea2ede3d
Sander <sandernollet24@gmail.com>
2021-06-19 16:24:14 +0200

    Added Uniform.php + References and Uniform.JPG

---
+++ Uniform.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Scouts Karel de Goede</title>
+
+<!--
+
+Breezed Template
+
+https://templatemo.com/tm-543-breezed
+
+-->
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-breezed.css">
+
+    <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+
+    </head>
+
+    <body>
+
+      <?php
+        include './Header.html';
+      ?>
+
+      <section class="section" id="about">
+          <div class="container">
+            <div class="section-heading">
+                <h2>Uniform</h2>
+            </div>
+            <!--https://getbootstrap.com/docs/4.0/layout/grid/ -->
+              <div class="row">
+                  <div class="col-lg-8 col-md-8 col-xs-12">
+                      <div class="left-text-content">
+                        <div class="col-lg-10 col-md-10 col-xs-12">
+                          <p>Zaterdagmiddag, ergens in het centrum van Brugge. Te midden de toeristen zie je enkele jongeren rondlopen op de Burg in Brugge. Maar ze zien er anders uit, ze lopen niet rond met een videocamera, fototoestel of zakken vol pralines. Neen, ze lopen rond met papiertjes in hun hand, leventjes, zo blijkt. Een geoefend oog had het natuurlijk al direct door dat het de scouts is die net bezig is met hun stadsspel, maar hoe zie je dat nu in godsnaam in een oogopslag?</p>
+                          <p><br></p>
+                          <p>Heel simpel, aan het uniform merk je het dat het om een groepje scouts en gidsen gaat. Met het dragen van het uniform tonen we dat we lid zijn van Scouts en Gidsen Vlaanderen. Met het uniform willen we iets gemeenschappelijks tonen, de scoutswaarden. Maar het uniform is niet zomaar een uniform, het is eigenlijk ook speelkledij. &ldquo;Bewegingskledij&rdquo; is misschien een beter woord dan &ldquo;uniform&rdquo;; uitnodigender, creatiever, vriendelijker.</p>
+                          <p><br></p>
+                          <p>Met het beige hemd vallen we natuurlijk al direct op, daaraan zien de mensen direct dat het om een scoutsgroep gaat. Maar we willen meer bereiken dan dat. We zijn meer dan scouts alleen, we zijn namelijk Scouts en Gidsen Karel De Goede. De mensen moeten ons (her)kennen. Daarom is er naast het scoutshemd ook nog de das met paarse-beige KDG-kleur, en ons bordeaux KDG-t-shirt.&nbsp;</p>
+                          <p><br></p>
+                          <h5>Hoe ziet ons uniform eruit?</h5>
+                          <p>Het basisuniform is voor wouters, jojo&rsquo;s, givers en leiding hetzelfde:</p>
+                          <br>
+                          <ul>
+                              <li> - beige scoutshemd,</li>
+                              <li> - groene lange broek, korte broek of rok,</li>
+                              <li> - das,</li>
+                              <li> - KDG-t-shirt (optioneel).</li>
+                          </ul>
+                          <br>
+                          <p>Voor de kapoenen volstaat een das.</p>
+                          <p><br></p>
+                          <p>T-shirts en dassen zijn verkrijgbaar bij de leiding. De rest van het uniform kan worden gekocht in de scoutsshop (via hopper.be)</p>
+                          <p><br></p>
+                          <p>Voor meer info over hoe de tekentjes moeten bevestigd worden aan het scoutshemd, verwijzen we je door naar de volgende website van scouts en gidsen vlaanderen: <a href="https://www.scoutsengidsenvlaanderen.be/ik-wil-naar-de-scouts/het-scoutsuniform"> https://www.scoutsengidsenvlaanderen.be/ik-wil-naar-de-scouts/het-scoutsuniform </a></p>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-xs-12">
+                      <div class="right-text-content">
+                        <br>
+                        <li><img class = "img-fill" src ="assets/images/Uniform.jpg" alt=""></li>
+                        <br>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+
+    <?php
+        include './Footer.html';
+      ?>
+
+  </body>
+</html>
